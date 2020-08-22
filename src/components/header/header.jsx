12034b83d9dbc95ccf0ca31b4da364af77ff3ae2
Tinashe <tinashejchiweshe@gmail.com>
2020-08-22 21:13:05 +0200

    styles updated

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,8 +4,7 @@ import 'tachyons'
 const Header = () => (
   <nav className="pa4 pa3-ns mb3 bb b--light-silver">
     <h1 
-      className="link dim black b f1 tc db mb3 mb2-ns" 
-      href="/" 
+      className="dim black b f1 tc db mb3 mb2-ns" 
       title="Naughts & Crosses"
     >
       Naughts & Crosses
